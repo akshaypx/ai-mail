dfb8e89f97b5f49cc2fd3280d01fc2f75261e300
akshaypx <akshaysrivastava46@gmail.com>
2025-01-05 20:10:28 +0530

    feat: app init and clerk init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { api, HydrateClient } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  void api.post.getLatest.prefetch();
+
+  return <Button>Hello world</Button>;
+}
